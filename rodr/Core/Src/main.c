20e0fbe1337bc65fa5fb32b8c094d904b551ff0a
RaQ1600 <drogowskimateusz8@gmail.com>
2025-05-26 19:29:18 +0200

    implemented getPressureTask

--- rodr/Core/Src/main.c
+++ rodr/Core/Src/main.c
@@ -26,6 +26,8 @@
 #include <string.h>
 #include <stdio.h>
 #include "lwip/api.h"
+
+#include "SEN0257.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -434,11 +436,7 @@ void StartDefaultTask(void const * argument)
 
 	  if (ctr % 100 == 0)
 	  {
-		  //packing float into int to put it into queue
-		  uint32_t packed_data;
-		  memcpy(&packed_data, &sensor_data, sizeof(float));
-
-//		  osMessagePut(UDPMsgQueueHandle, packed_data, osWaitForever);
+		  //mayne free mutex that allows for udp sending here??
 	  }
 
 	  ++ctr;
@@ -594,13 +592,73 @@ void StartUDPStreamTask(void const * argument)
 void startGetPressureTask(void const * argument)
 {
   /* USER CODE BEGIN startGetPressureTask */
+	float voltage = 0;
+	float medianV = 0;
+
+	float voltage_samples[SHORT_WIN];
+	int short_id = 0;
+	bool short_filed = false;
+
+	float long_term_filtered[LONG_WIN];
+	int long_id = 0;
+	bool long_filled = false;
+
+	float fir_avg_buff[LONG_WIN] = { 0 };
+	int fir_avg_id= 0;
+	bool fir_avg_filled = false;
+
+	float long_fir_avg = 0.0f;
+	float long_fir = 0.0f;
+
+	float pressure = 0;
   /* Infinite loop */
-  for(;;)
-  {
+	for(;;)
+	{
+		voltage = SEN0257_readStableAnalog(5, &hadc1) * 5.0f / 4095.0f;
+
+		voltage_samples[short_id] = voltage;
+		short_id = GET_AVG_ID(short_id, SHORT_WIN);
+
+		if (short_id == 0) short_filed = true;
 
 
-    osDelay(1);
-  }
+		if (short_filed)
+		{
+			medianV = SEN0257_getMedian(voltage_samples, SHORT_WIN);
+
+			long_term_filtered[long_id] = medianV;
+			long_id = GET_AVG_ID(long_id, LONG_WIN);
+
+			if (long_id == 0) long_filled = true;
+		}
+
+		fir_avg_buff[fir_avg_id] = medianV;
+		fir_avg_id = GET_AVG_ID(fir_avg_id, LONG_WIN);
+
+		if (fir_avg_id == 0) fir_avg_filled = true;
+
+		//calculate only when buffer is full
+		if (fir_avg_filled)
+		{
+			float sum = 0.0f;
+			for (int i = 0; i < LONG_WIN; i++)
+				sum += fir_avg_buff[i];
+
+			long_fir_avg = sum / LONG_WIN;
+		}
+		else
+			long_fir_avg = medianV;  // use current val until buff filled
+
+		pressure = (long_fir_avg - OFFSET) * 250.0f;//this is output so it should be queued
+
+		//packing float into int to put it into queue
+		uint32_t packed_data;
+		memcpy(&packed_data, &pressure, sizeof(float));
+
+		osMessagePut(pressureQueueHandle, packed_data, osWaitForever);
+
+		osDelay(10);//should be changed to fit whatever we want to do
+	}
   /* USER CODE END startGetPressureTask */
 }
 
